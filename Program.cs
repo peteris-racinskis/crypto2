80cccd090c2212a9abee6e2d689613c4cd27f2ce
Your Name <peterisracinskis@gmail.com>
2021-06-15 22:04:50 +0300

    can create and read .pem certificates

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace crypto2
+{
+    class Program
+    {
+        
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"args {args[0]}");
+            using (RSA rsa = RSA.Create())
+            {
+                rsa.KeySize = 2048;
+                var req = new CertificateRequest("cn=foobar", rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+                var cert = req.CreateSelfSigned(DateTimeOffset.Now, DateTimeOffset.Now.AddYears(5));
+                var certBytes = cert.GetRawCertData();
+                var key  = cert.GetRSAPrivateKey().ExportRSAPrivateKey();
+                var pemCoded = PemEncoding.Write("CERTIFICATE",certBytes);
+                var pemCodedKey = PemEncoding.Write("RSA PRIVATE KEY",key);
+                File.WriteAllText("coded.pem",new string(pemCoded));
+                File.WriteAllText("private_key.pem",new string(pemCodedKey));
+                var cert_from_file = X509Certificate2.CreateFromPemFile("coded.pem", "private_key.pem");
+                var cert_from_chararr = X509Certificate2.CreateFromPem(pemCoded,pemCodedKey);
+            };
+        }
+    }
+}
