58adaa8c2b3f17bd46985122579a5972d20755cc
Your Name <peterisracinskis@gmail.com>
2021-06-27 23:34:21 +0300

    fixed launch script

--- launcher.sh
+++ launcher.sh
@@ -9,34 +9,35 @@
 #  ^-- inspect results in outputs/
 # ./launcher.sh decrypt
 #  ^-- inspect results in outputs/
-
-outfile = ""
-comm = ""
-args = ""
-case $1 in
+echo $0
+case $0 in
     "generate")
-        outfile = "--outfile outputs/test-exist"
-        comm = "generate"
-        args = "outputs/test-config.txt --priv-key outputs/test-exist-private-key.pem"
+        outfile="--outfile outputs/test-exist"
+        test_command="generate"
+        args="outputs/test-config.txt --priv-key outputs/test-exist-private-key.pem"
         ;;
     "validate-pass")
-        comm = "validate"
-        args = "outputs/test-exist-cert.pem"
+        test_command="validate"
+        args="outputs/test-exist-cert.pem"
         ;;
     "validate-fail")
-        comm = "validate"
-        args = "outputs/garbled.pem"
+        test_command="validate"
+        args="outputs/garbled.pem"
         ;;
     "encrypt")
-        outfile = "outputs/encrypted.bin"
-        comm = "encrypt"
-        args = "outputs/original.txt outputs/test-exist-cert.pem"
+        outfile="outputs/encrypted.bin"
+        test_command="encrypt"
+        args="outputs/original.txt outputs/test-exist-cert.pem"
         ;;
     "decrypt")
-        outfile = "outputs/decrypted.txt"
-        comm = "decrypt"
-        args = "outputs/encrypted.bin outputs/test-exist-private-key.pem"
+        outfile="outputs/decrypted.txt"
+        test_command="decrypt"
+        args="outputs/encrypted.bin outputs/test-exist-private-key.pem"
         ;;
-arguments="./crypto2 $outfile $command $args"
+    * )
+        echo "Choose a test_command"
+        ;;
+esac
+arguments="./crypto2 $outfile $test_command $args"
 
 exec $arguments
